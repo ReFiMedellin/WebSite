07b98d76e20fa06bfac40be6ec6f0bcf33ea3093
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-09-03 11:08:10 -0500

    feat: partial structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import Image from 'next/image';
+import Logo from '@/assets/images/logo.png';
+import Logo2 from '@/assets/images/logo2.png';
+import Link from 'next/link';
 
 // const a =
 const cards = [1, 2, 3, 4, 5, 6];
@@ -7,15 +10,20 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between'>
       <div className='flex flex-row justify-center items-center h-screen bg-neutral-900 w-full'>
-        <div className='w-full h-full grid grid-cols-2 '>
+        <div className='h-full w-5/6 flex flex-row justify-center items-center'>
           <div className='flex justify-center items-center w-full h-full'>
-            <div className='w-5/6 flex flex-col gap-5'>
-              <h1 className='text-white font-bold text-6xl'> Refi Medellín</h1>
-              <p className='text-white'>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio
-                labore ex voluptatem delectus quia, magni eum sit animi expedita
-                explicabo eius ut reiciendis necessitatibus iste quaerat esse
-                praesentium totam sapiente.
+            <div className='text-white w-5/6 flex flex-col gap-5'>
+              <h1 className='font-bold text-6xl'>
+                ¡Bienvenido a ReFi Medellín!
+              </h1>
+              <p>
+                El primer Nodo colombiano de ReFi DAO encargado de promover
+                proyectos ReFi de la región.
+              </p>
+              <p>
+                En ReFi Medellín, creemos en los proyectos regenerativos y el
+                impacto transformador que estos tienen. ¡Es hora de pensar
+                diferente y regenerarnos juntos!
               </p>
               <div className='flex flex-row gap-2'>
                 <button className='px-4 py-2 w-full rounded-md bg-purple-700 text-white font-bold text-lg'>
@@ -27,12 +35,10 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className='flex justify-center items-center w-full h-full'>
-            <div className='bg-amber-600 h-1/2 w-1/2' />
-          </div>
+          <Image height={420} src={Logo} alt='Logo Refi' />
         </div>
       </div>
-      <div className='h-screen w-full bg-purple-400 flex flex-col justify-center items-center gap-10'>
+      <div className='h-screen w-full bg-purple-700 flex flex-col justify-center items-center gap-10'>
         <h1 className='font-bold w-full text-center text-white text-5xl'>
           Proyectos
         </h1>
@@ -40,73 +46,82 @@ export default function Home() {
           {cards.map((card, index) => (
             <div
               key={index}
-              className='h-40 shadow-md w-full rounded-md p-5 bg-blue-300'
+              className='py-6 flex flex-row gap-2 justify-center items-center shadow-md w-full rounded-md p-5 bg-white'
             >
-              Card
+              <Image src={Logo2} alt='logo refi' height={120} />
+              <div>
+                <h1 className='text-lg font-bold'>
+                  Engage the local community
+                </h1>
+                <p>
+                  Host four ReFi Medellín events over the next year. These
+                  events will include expert guest speakers in ReFi, Web3, and
+                  urban challenges in Latin American cities.
+                </p>
+              </div>
             </div>
           ))}
         </div>
       </div>
-      <div className='h-screen grid grid-cols-2 justify-center items-center bg-black w-full'>
-        <div className='w-full h-full flex justify-center items-center'>
-          <div className='bg-amber-500 w-1/2 h-1/2' />
-        </div>
-        <div className='text-white '>
-          <div className='w-5/6 flex flex-col gap-5 h-5/6'>
-            <h1 className='text-4xl font-bold'>lorem ipsum et apostel</h1>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Laudantium sunt dolor cum repudiandae corrupti consectetur aperiam
-              vero eaque neque, mollitia velit voluptate error illo debitis
-              ipsam fuga fugiat culpa accusamus. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Natus vero impedit incidunt
-              perspiciatis quidem blanditiis cumque iure quos! Vero expedita
-              facere soluta molestias deserunt iure dolorum sequi perferendis
-              modi dignissimos.
-            </p>
-            <button className='bg-purple-700 rounded-md text-white font-bold  text-lg px-12 py-2'>
-              Lorem
-            </button>
+      <div className='h-screen flex justify-center items-center bg-black w-full'>
+        <div className='h-full w-5/6 flex flex-row justify-center gap-10 items-center'>
+          <Image src={Logo} alt='refi logo' height={480} />
+          <div className='text-white flex gap-5  flex-col justify-center items-center h-full  w-full'>
+              <h1 className='text-4xl font-bold'>
+                ReFi Medellín Join the movement towards a more sustainable and
+                equitable future in Medellín, Colombia
+              </h1>
+              <p>
+                We are excited to announce the formation of a ReFi Local Node in
+                Medellín, led by Juan Giraldo, Tereza Bizkova, Alejandro Soto,
+                Eamonn, Sergio, Green Digital guardians and Dotlabs() to nurture
+                community conversations around Web3 enabled
+                <br />
+                regenerative-oriented-innovations (ROIs). Our aim is to empower
+                the youth in Medellín to address some of our city&apos;s most
+                pressing challenges through hackathons, incubation, and
+                investment. These challenges include poverty, inequality, youth
+                unemployment, and a general lack of access to resource flows
+                such as energy, water, sanitation, housing, and education.
+              </p>
+              <button className='w-full bg-purple-700 rounded-md text-white font-bold  text-lg px-12 py-2'>
+                Conocenos
+              </button>
           </div>
         </div>
       </div>
-      <div className='h-screen grid grid-cols-2 justify-center items-center w-full'>
-        <div className='w-full h-full flex justify-center items-center gap-5'>
-          <div className='w-5/6 h-5/6 flex flex-col gap-5 justify-center items-start  '>
-            <h1>Lorem Ipsum</h1>
+      <div className='h-screen flex justify-center items-center w-full'>
+        <div className='h-full w-5/6 flex flex-row justify-center gap-10 items-center'>
+          <div className='flex flex-col gap-5'>
+            <h1 className='text-4xl font-bold'>¡Apoya el cambio!</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Blanditiis voluptate consectetur dolorem fugit neque temporibus
-              cupiditate praesentium sit omnis, consequatur at error ad cumque
-              saepe expedita iusto quam dolore similique?
+              Our aim is to empower the youth in Medellín to address some of our
+              city&apos;s most pressing challenges through hackathons,
+              incubation, and investment. These challenges include poverty,
+              inequality, youth unemployment, and a general lack of access to
+              resource flows such as energy, water, sanitation, housing, and
+              education.
+              <br />
+              We kindly ask for your support and donations, so we can start our
+              journey of youth-led education for regeneration!
+              <br />
+              Wallet (Ethereum): 0xd4AC6c14B4C96F7e66049210F56cb07468028d4e
             </p>
             <button className='bg-purple-700 text-white rounded-md w-full font-bold px-8 py-4'>
-              Lorem
+              Apoyar
             </button>
           </div>
-        </div>
-        <div className='w-full h-full flex justify-center items-center'>
-          <div className='bg-amber-600 h-1/2 w-1/2' />
+        <Image src={Logo2} alt='refi logo' height={480} />
         </div>
       </div>
       <footer className=' bg-slate-200 w-full '>
         <div className='flex py-60 px-14 flex-row justify-between items-center gap-20'>
           <div className='flex flex-col justify-center items-center w-1/2'>
-            <h1 className='text-4xl w-full text-start font-bold'>Footer</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Blanditiis qui aliquid ex voluptatibus debitis asperiores laborum
-              ullam obcaecati praesentium quaerat doloribus voluptates, nobis
-              nemo dolore eveniet distinctio consectetur culpa suscipit.
-            </p>
-          </div>
-          <div className='w-1/2 text-center text-blue-500 flex flex-col gap-4'>
-            <p>Some thing</p>
-            <p>Some thing</p>
+            <h1 className='text-4xl w-full text-start font-bold'>Contacto</h1>
           </div>
         </div>
-        <div className='h-[20vh] bg-purple-300 flex justify-center items-center text-center'>
-            <p>®2023 Refi medellin, made with 🩵 by Refi medellín team</p>
+        <div className='h-[20vh] bg-gray-700 text-white  flex justify-center items-center text-center'>
+          <p>®2023 Refi medellin, made with 🩵 by Refi medellín team</p>
         </div>
       </footer>
     </main>
