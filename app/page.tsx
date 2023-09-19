74fab9bcc522c82b2779cc211775b47eb36d6a02
Juan José Giraldo <91024217+Juancamp1987@users.noreply.github.com>
2023-09-19 00:23:05 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -474,11 +474,6 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <Image
-            className='absolute bottom-0 w-[100vw] left-0'
-            src={BordeBottom}
-            alt='Medellin'
-          />
         </div>
       </section>
       <section className='min-h-screen py-20 relative bg-[#F1F0FF] flex justify-center items-center w-full'>
@@ -531,6 +526,11 @@ export default function Home() {
             src={LogoMan}
             alt='refi logo'
             height={480}
+          />
+          <Image
+            className='absolute bottom-0 w-[100vw] left-0'
+            src={BordeBottom}
+            alt='Medellin'
           />
         </div>
       </section>
