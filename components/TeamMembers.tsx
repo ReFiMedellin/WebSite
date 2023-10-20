4eef309c7213e69e2ac57a51658e1cd7b2138343
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-10-19 20:59:16 -0500

    fix: minimize teammember size

--- components/TeamMembers.tsx
+++ components/TeamMembers.tsx
@@ -122,10 +122,10 @@ const TeamMembers = () => {
       {team.map((member, index) => (
         <div
           key={index}
-          className='relative h-76 w-64 pt-5 rounded-md shadow-lg flex flex-col gap-2 bg-slate-200 group hover:bg-slate-300'
+          className='relative h-76 md:h-[17rem] w-64 md:w-52 pt-5 overflow-y-hidden rounded-md shadow-lg flex flex-col gap-2 bg-slate-200 group hover:bg-slate-300'
         >
           <div className='px-5 text-center'>
-            <h1 className='text-start w-full font-bold text-lg'>
+            <h1 className='text-start w-full font-bold text-sm'>
               {member.name}
             </h1>
             <p className='w-full text-start font-light text-sm text-slate-700'>
@@ -135,7 +135,7 @@ const TeamMembers = () => {
           <Image
             src={member.imageSrc}
             alt='Refi member'
-            className='w-full h-full rounded-b-md'
+            className='w-full max-h-full rounded-b-md'
           />
           <div className='bg-slate-200 absolute bottom-0 left-0 w-full h-12 rounded-b-md opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
             <div className='flex text-sm h-full flex-row justify-center items-center gap-5'>
