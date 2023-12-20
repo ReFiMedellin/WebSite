fbec2cbbf31309c307f763f418fc394c7c50c98c
Another <joseluismanco37@gmail.com>
2023-12-19 20:52:46 -0500

    chore: text

--- components/loanPanel/FundLend.tsx
+++ components/loanPanel/FundLend.tsx
@@ -151,11 +151,11 @@ function FundLend () {
                     required: 'Este campo es requerido',
                     min: {
                       value: 0,
-                      message: 'El monto debe ser mayor a 0'
+                      message: 'La cantidad de meses debe ser mayor a 0'
                     },
                     max: {
                       value: 12,
-                      message: 'El monto debe ser menor a 12'
+                      message: 'La cantidad de meses debe ser menor o igual a 12'
                     }
                   }}
                   render={({ field }) => (
