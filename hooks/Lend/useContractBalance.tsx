73b9b9aad45fe2e44d4837091657d1dd15926166
Another <joseluismanco37@gmail.com>
2024-01-14 19:39:13 -0500

    fix: false totalValue

--- hooks/Lend/useContractBalance.tsx
+++ hooks/Lend/useContractBalance.tsx
@@ -3,14 +3,14 @@ import { erc20ABI, useAccount, useContractRead } from 'wagmi'
 import { useNetworkContract } from './useNetworkContract'
 
 function useContractBalance () {
-  const { tokenAddress } = useNetworkContract()
+  const { tokenAddress, lendAddress } = useNetworkContract()
 
   const balance = useContractRead({
     address: tokenAddress,
     abi: erc20ABI,
     functionName: 'balanceOf',
     watch: true,
-    args: [tokenAddress as Address]
+    args: [lendAddress as Address]
   })
   return balance
 }
