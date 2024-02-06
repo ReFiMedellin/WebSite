5907b52f4dc4deb36140f7e1ef191fc6cd9ba0c6
Another <joseluismanco37@gmail.com>
2024-02-05 19:49:23 -0600

    chore: add currency

--- components/loanPanel/AnimatedPrice.tsx
+++ components/loanPanel/AnimatedPrice.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { motion, useMotionValue, useTransform, animate } from 'framer-motion'
+import React from 'react';
+import { useEffect } from 'react';
+import { motion, useMotionValue, useTransform, animate } from 'framer-motion';
+import { currencies } from '@/constants';
+import { useNetwork } from 'wagmi';
 
-function AnimatedPrice ({ price }: { price: number }) {
-  const count = useMotionValue(0)
-  const rounded = useTransform(count, latest => Math.round(latest))
-
+function AnimatedPrice({ price }: { price: number }) {
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+  const { chain } = useNetwork();
+  const currentCurrency = currencies[chain?.id as keyof typeof currencies];
   useEffect(() => {
-    const controls = animate(count, price)
-    return controls.stop
-  }, [price])
+    const controls = animate(count, price);
+    return controls.stop;
+  }, [price]);
   return (
     <div className='text-2xl flex flex-row gap-1 font-bold'>
-      $<motion.div>{rounded}</motion.div>
+      $<motion.div>{rounded}</motion.div> {currentCurrency}
     </div>
-  )
+  );
 }
 
-export default AnimatedPrice
+export default AnimatedPrice;
