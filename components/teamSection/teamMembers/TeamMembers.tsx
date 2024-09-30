bbceccd2e00b1670e44cb40282c3cb8df2ab2826
simon0820s <simon0820s@gmail.com>
2024-09-30 16:39:25 -0500

    feat(teamSection): show cards on carousel

--- components/teamSection/teamMembers/TeamMembers.tsx
+++ components/teamSection/teamMembers/TeamMembers.tsx
@@ -1,26 +1,43 @@
 import React from "react";
+import Autoplay from "embla-carousel-autoplay"
 import TeamMemberComponent from "./teamMember/TeamMember";
-import {type TeamMemberType } from "@/types/members";
+import { type TeamMemberType } from "@/types/members";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 
 type Props = {
-  members: TeamMemberType[][];
+  members: TeamMemberType[];
   opaque: boolean;
 };
 
 const TeamMembers = ({ members, opaque }: Props) => {
   return (
-    <div className="flex flex-col gap-5">
-      {members.map((membersRow, index) => (
-        <div
-          key={index}
-          className="flex justify-center items-center flex-wrap gap-5"
-        >
-          {membersRow.map((member, index) => (
-            <TeamMemberComponent key={index} member={member} opaque={opaque} />
-          ))}
-        </div>
-      ))}
-    </div>
+    <Carousel className="w-64 md:w-52"
+      opts={{
+        dragFree: true
+      }}
+      plugins={[
+        Autoplay({
+          delay: 2000,
+        }),
+      ]}
+    >
+      <CarouselContent>
+        {members.map((member, index) => (
+          <CarouselItem key={index}>
+            <TeamMemberComponent member={member} opaque={opaque} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="hidden md:inline-flex" />
+      <CarouselNext className="hidden md:inline-flex" />
+    </Carousel>
   );
 };
 
