320d50b4077e6430d6ee2ab5676b9b86bc52e2ee
simon0820s <simon0820s@gmail.com>
2024-10-12 15:18:25 -0500

    feat(parthners): render parthners using new cards

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -1,19 +1,10 @@
 import React from "react";
-import { FaTelegramPlane } from "react-icons/fa";
-import Link from "next/link";
-import Image from "next/image";
-import { RxInstagramLogo, RxLinkedinLogo, RxTwitterLogo } from "react-icons/rx";
-import Juan from "@/assets/images/PFP-Juan.webp";
-import Tereza from "@/assets/images/PFP-Tereza.webp";
-import Alejandro from "@/assets/images/PFP-Alejandro.webp";
-import xflypeztyc from "@/assets/images/PFP-0xflypeztic.webp";
-import Ximena from "@/assets/images/PFP-Ximena.webp";
-import Eamon from "@/assets/images/PFP-Eamon.webp";
-import Edward from "@/assets/images/PFP-Edward.webp";
 import GDG from "@/assets/images/PFP-Green Digital Guardians.webp";
-// import Inkom from '@/assets/images/PFP-Inkom.webp'
 import DotLabs from "@/assets/images/PFP-Dotlabs.webp";
 import { useTranslations } from "next-intl";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
+import TeamMemberComponent from "./teamSection/teamMembers/teamMember/TeamMember";
+import Autoplay from "embla-carousel-autoplay";
 
 const Partners = () => {
   const t = useTranslations("Landing");
@@ -54,76 +45,31 @@ const Partners = () => {
   ];
 
   return (
-    <div className="flex flex-col items-center gap-12">
+    <div className="flex flex-col items-center gap-8">
       <h2 className="font-bold text-4xl text-black">{t("team.partners")}</h2>
-      <div className="flex-row flex flex-wrap justify-center items-center gap-5">
-        {partners.map((member, index) => (
-          <div
-            key={index}
-            className="relative h-76 md:h-[17rem] w-64 md:w-52 pt-5 overflow-y-hidden rounded-md shadow-lg flex flex-col gap-2 bg-slate-200 group hover:bg-slate-300"
-          >
-            <div className="px-5 text-center">
-              <h1 className="text-start w-full font-bold text-sm">
-                {member.name}
-              </h1>
-              <p className="w-full text-start font-light text-sm text-slate-700">
-                {member.role}
-              </p>
-            </div>
-            <Image
-              src={member.imageSrc}
-              alt="Refi member"
-              className="w-full max-h-full rounded-b-md"
-            />
-            <div className="bg-slate-200 absolute bottom-0 left-0 w-full h-12 rounded-b-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <div className="flex text-sm h-full flex-row justify-center items-center gap-5">
-                <Link
-                  className="logo_teammember"
-                  target="_blank"
-                  href={
-                    member.socialLinks.twitter ? member.socialLinks.twitter : ""
-                  }
-                >
-                  <RxTwitterLogo />
-                </Link>
-                <Link
-                  className="logo_teammember"
-                  target="_blank"
-                  href={
-                    member.socialLinks.instagram
-                      ? member.socialLinks.instagram
-                      : ""
-                  }
-                >
-                  <RxInstagramLogo />
-                </Link>
-                <Link
-                  className="logo_teammember"
-                  target="_blank"
-                  href={
-                    member.socialLinks.telegram
-                      ? member.socialLinks.telegram
-                      : ""
-                  }
-                >
-                  <FaTelegramPlane />
-                </Link>
-                <Link
-                  className="logo_teammember"
-                  target="_blank"
-                  href={
-                    member.socialLinks.linkedin
-                      ? member.socialLinks.linkedin
-                      : ""
-                  }
-                >
-                  <RxLinkedinLogo />
-                </Link>
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 2000,
+          }),
+        ]}
+        className="w-full max-w-sm md:max-w-lg lg:max-w-4xl xl: xl:max-w-5xl 2xl:max-w-6xl"
+      >
+        <CarouselContent>
+          {partners.map((member, index) => (
+            <CarouselItem
+              key={index}
+              className="md:basis-1/2 lg:basis-1/3 2xl:basis-1/4"
+            >
+              <div className="flex items-center justify-center py-2">
+                <TeamMemberComponent member={member} />
               </div>
-            </div>
-          </div>
-        ))}
-      </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="hidden sm:inline-flex" />
+        <CarouselNext className="hidden sm:inline-flex" />
+      </Carousel>
     </div>
   );
 };
