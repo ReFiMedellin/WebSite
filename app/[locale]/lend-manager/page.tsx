ba039b850584ae7233f9673e45dc22ff0217b6da
simon0820s <simon0820s@gmail.com>
2024-11-02 11:29:58 -0500

    feat(lendManager): create component to show current requests

--- app/[locale]/lend-manager/page.tsx
+++ app/[locale]/lend-manager/page.tsx
@@ -1,25 +1,26 @@
-'use client';
-import { AddToken } from '@/components/lendV2/AddToken';
-import { DecreaseQuota } from '@/components/lendV2/DecreaseQuota';
-import LendsManager from '@/components/lendV2/LendsManager';
-import { ProtocolInfo } from '@/components/lendV2/ProtocolInfo';
-import { QuotaManager } from '@/components/lendV2/QuotaManager';
-import { UsersManager } from '@/components/lendV2/UsersManager';
-import { Button } from '@/components/ui/button';
+"use client";
+import { AddToken } from "@/components/lendV2/AddToken";
+import { DecreaseQuota } from "@/components/lendV2/DecreaseQuota";
+import LendsManager from "@/components/lendV2/LendsManager";
+import { ProtocolInfo } from "@/components/lendV2/ProtocolInfo";
+import { QuotaManager } from "@/components/lendV2/QuotaManager";
+import RequestManager from "@/components/lendV2/RequestManager";
+import { UsersManager } from "@/components/lendV2/UsersManager";
+import { Button } from "@/components/ui/button";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from '@/components/ui/select';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { toast } from '@/components/ui/use-toast';
-import { Chains } from '@/constants/chains';
-import { useIsAdmin } from '@/hooks/LendV2/useIsAdmin';
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
-import { useNetwork, useSwitchNetwork } from 'wagmi';
+} from "@/components/ui/select";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { toast } from "@/components/ui/use-toast";
+import { Chains } from "@/constants/chains";
+import { useIsAdmin } from "@/hooks/LendV2/useIsAdmin";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { useNetwork, useSwitchNetwork } from "wagmi";
 
 function Page() {
   const { data, isLoading } = useIsAdmin();
@@ -35,43 +36,42 @@ function Page() {
   useEffect(() => {
     const currentChain =
       chain?.id === Chains.celo
-        ? 'celo'
+        ? "celo"
         : chain?.id === Chains.optimism
-        ? 'optimism'
+        ? "optimism"
         : chain?.id === Chains.polygon
-        ? 'polygon'
+        ? "polygon"
         : chain?.id === Chains.sepolia
-        ? 'sepolia'
+        ? "sepolia"
         : chain?.id === Chains.arbitrum
-        ? 'arbitrum'
+        ? "arbitrum"
         : null;
     setSelectedChain(currentChain);
-
   }, [chain]);
   if (isLoading) {
     return (
-      <main className='flex px-5  py-32 gap-4 lg:px-20  bg-[#1B2731] min-h-screen justify-center items-center'>
-        <h1 className='font-bold text-4xl text-white lg:text-8xl'>
+      <main className="flex px-5  py-32 gap-4 lg:px-20  bg-[#1B2731] min-h-screen justify-center items-center">
+        <h1 className="font-bold text-4xl text-white lg:text-8xl">
           Loading...
         </h1>
       </main>
     );
   }
-  if (!data && chain?.name != 'sepolia') {
-    return (
-      <main className='flex px-5  py-32 gap-4 lg:px-20  bg-[#1B2731] min-h-screen justify-center items-center'>
-        <h1 className='font-bold text-4xl text-white lg:text-8xl'>
-          Unauthorized
-        </h1>
-        ;
-      </main>
-    );
-  }
+  // if (!data && chain?.name != 'sepolia') {
+  //   return (
+  //     <main className='flex px-5  py-32 gap-4 lg:px-20  bg-[#1B2731] min-h-screen justify-center items-center'>
+  //       <h1 className='font-bold text-4xl text-white lg:text-8xl'>
+  //         Unauthorized
+  //       </h1>
+  //       ;
+  //     </main>
+  //   );
+  // }
   const handleNetworkChange = async (value: keyof typeof Chains) => {
     const desiredChainId = Chains[value];
     toast({
-      title: 'Tip',
-      description: 'Recuerda aceptar el cambio de red en tu billetera',
+      title: "Tip",
+      description: "Recuerda aceptar el cambio de red en tu billetera",
     });
     await switchNetworkAsync?.(desiredChainId);
 
@@ -87,9 +87,9 @@ function Page() {
   };
 
   return (
-    <main className='lendManager px-5 text-white py-32 gap-4 lg:px-20  bg-[#1B2731] min-h-screen justify-center items-center'>
-      <div className='flex flex-row gap-4 w-full items-end'>
-        <div className='flex flex-col gap-2  place-self-start'>
+    <main className="lendManager px-5 text-white py-32 gap-4 lg:px-20  bg-[#1B2731] min-h-screen justify-center items-center">
+      <div className="flex flex-row gap-4 w-full items-end">
+        <div className="flex flex-col gap-2  place-self-start">
           <h4>Selecciona la red</h4>
           <Select
             key={selectedChain}
@@ -97,55 +97,56 @@ function Page() {
             onValueChange={handleNetworkChange}
           >
             <SelectTrigger>
-              <SelectValue placeholder='Red' />
+              <SelectValue placeholder="Red" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value='celo'>Celo</SelectItem>
-              <SelectItem value='optimism'>Optimism</SelectItem>
-              <SelectItem value='polygon'>Polygon</SelectItem>
-<SelectItem value='arbitrum'>Arbitrum</SelectItem>
+              <SelectItem value="celo">Celo</SelectItem>
+              <SelectItem value="optimism">Optimism</SelectItem>
+              <SelectItem value="polygon">Polygon</SelectItem>
+              <SelectItem value="arbitrum">Arbitrum</SelectItem>
             </SelectContent>
           </Select>
         </div>
-        <Button onClick={() => push('/community')} variant='outline'>
+        <Button onClick={() => push("/community")} variant="outline">
           Lending dashboard
         </Button>
         {!!isAdmin && !isAdminLoading && (
-          <Button variant='secondary' className='justify-self-end'>
+          <Button variant="secondary" className="justify-self-end">
             Admin manager
           </Button>
         )}
       </div>
       <div
         style={{
-          gridArea: 'manager',
+          gridArea: "manager",
         }}
-        className='flex flex-col gap-4  w-full h-full'
+        className="flex flex-col gap-4  w-full h-full"
       >
         <LendsManager />
+        <RequestManager/>
         <UsersManager />
       </div>
       <div
         style={{
-          gridArea: 'info',
+          gridArea: "info",
         }}
-        className='flex flex-col gap-4  w-full h-full'
+        className="flex flex-col gap-4  w-full h-full"
       >
         <ProtocolInfo />
 
-        <Tabs defaultValue='increase'>
-          <TabsList className='grid w-full grid-cols-3'>
-            <TabsTrigger value='increase'>Increase Quota</TabsTrigger>
-            <TabsTrigger value='decrease'>Decrease Quota</TabsTrigger>
-            <TabsTrigger value='token'>Token manager</TabsTrigger>
+        <Tabs defaultValue="increase">
+          <TabsList className="grid w-full grid-cols-3">
+            <TabsTrigger value="increase">Increase Quota</TabsTrigger>
+            <TabsTrigger value="decrease">Decrease Quota</TabsTrigger>
+            <TabsTrigger value="token">Token manager</TabsTrigger>
           </TabsList>
-          <TabsContent value='token'>
+          <TabsContent value="token">
             <AddToken />
           </TabsContent>
-          <TabsContent value='increase'>
+          <TabsContent value="increase">
             <QuotaManager />
           </TabsContent>
-          <TabsContent value='decrease'>
+          <TabsContent value="decrease">
             <DecreaseQuota />
           </TabsContent>
         </Tabs>
---
+++ components/lendV2/RequestManager/index.tsx
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { LendStatus, useGetAllLends } from "@/hooks/LendV2/useGetAllLends";
+import { formatUnits } from "viem";
+import { Button } from "@/components/ui/button";
+import abreviarHash from "@/functions/abreviateHash";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { PiBroomBold } from "react-icons/pi";
+import { RequestStatus } from "@/hooks/LendV2/useGetAllRequests";
+
+function LendsManager() {
+  const [page, setPage] = useState(0);
+  const [lendStatus, setLendStatus] = useState(LendStatus.ACTIVE);
+  const {
+    data,
+    loading: isLoading,
+    error: isError,
+  } = useGetAllLends(page, lendStatus);
+  return (
+    <Card
+      style={{
+        gridArea: "lends",
+      }}
+      className="align-top w-full"
+    >
+      <CardHeader>
+        <div className="flex justify-between">
+          <CardTitle>Current requests</CardTitle>
+
+          <Select
+            onValueChange={(value: LendStatus) => setLendStatus(value)}
+            defaultValue={LendStatus.ACTIVE}
+          >
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Filters" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value={RequestStatus.ALL}>All request</SelectItem>
+                <SelectItem value={RequestStatus.SIGNED}>
+                  Signed requests
+                </SelectItem>
+                <SelectItem value={RequestStatus.COMPLETED}>
+                  Completed requests
+                </SelectItem>
+                <SelectItem value={LendStatus.ACTIVE}>Active loans</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableCaption>A list of the whole requests.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>User</TableHead>
+              <TableHead>Signatures / References</TableHead>
+            </TableRow>
+          </TableHeader>
+          {!isLoading && !isError && (
+            <TableBody>
+              {(data.lendings as any[]).map((lend, key) => (
+                <TableRow key={key}>
+                  <TableCell className="flex flex-row gap-2 items-center ">
+                    <Button
+                      className="w-8 h-8"
+                      variant={"outline"}
+                      onClick={() => navigator.clipboard.writeText(lend.lender)}
+                    >
+                      <FontAwesomeIcon icon={faCopy} />
+                    </Button>
+                    {abreviarHash(lend.lender)}
+                  </TableCell>
+                  <TableCell className="font-medium">
+                    NA / NA
+                  </TableCell>
+                  <TableCell className="flex flex-row gap-2 items-center ">
+                    <Button className="p-2" variant={"outline"}>
+                      <PiBroomBold className="text-xl text-teal-950" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          )}
+        </Table>
+      </CardContent>
+      <CardFooter className="flex flex-row gap-4">
+        <Button
+          disabled={isLoading || !data || data.lendings.length === 0}
+          onClick={() => setPage(page + 1)}
+        >
+          Next
+        </Button>
+
+        <Button disabled={page == 0} onClick={() => setPage(page - 1)}>
+          Previous
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default LendsManager;
