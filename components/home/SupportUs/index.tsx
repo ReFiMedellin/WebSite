3568dad759d323c4496eb03f953c8486d2d8a047
simon0820s <simon0820s@gmail.com>
2024-12-18 09:08:37 -0500

    chore(cCop): remove unused address variable

--- components/home/SupportUs/index.tsx
+++ components/home/SupportUs/index.tsx
@@ -10,7 +10,6 @@ import { PiCopyBold } from "react-icons/pi";
 export default function SupportUs() {
   const t = useTranslations("Landing");
   const address = "0xd4AC6c14B4C96F7e66049210F56cb07468028d4e";
-  const cCopAddress = "0x8A567e2aE79CA692Bd748aB832081C45de4041eA";
 
   const copyToClipboard = (selectedAddress: string) => {
     navigator.clipboard.writeText(selectedAddress);
